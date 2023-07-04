5352b2adf9d0e85b06351ef15d54e5fb8395ce9b
Sudeer <54216092+sudeer13@users.noreply.github.com>
2023-07-04 15:34:37 +0530

    Update code

--- reference-attributes.tf
+++ reference-attributes.tf
@@ -2,24 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["${aws_eip.lb.public_ip}/32"]
-  }
-
+resource "aws_instance" "web" {
+  ami           = "ami-022e1a32d3f742bd8"
+  instance_type = "t2.micro"
+  key_name = "linux-key"
+  vpc_security_group_ids = [aws_security_group.Allow_SSH.id]
   tags = {
-    Name = "allow_tls"
+    Name = "Webserver"
   }
 }
 
-resource "aws_eip" "lb" {
-  domain   = "vpc"
+resource "aws_security_group" "Allow_SSH" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"  
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  
+  }
+  tags = {
+    Name = "Allow_ssh"
+  }
+}
+
+resource "aws_eip" "public_IP" {
+  domain   = "vpc"  
+}
+
+resource "aws_eip_association" "ec2-eip-association" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.public_IP.id
 }
